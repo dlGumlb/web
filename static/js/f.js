5f5d2eaedf297dae508cb054de2830b4f52d53aa
Eason T <easont368777@gmail.com>
2024-02-14 20:00:17 -0500

    change

--- static/js/f.js
+++ static/js/f.js
@@ -1,3 +1,16 @@
+
 function openmenu() {
-    document.getElementById('menu_content').display = 'block'
+    let x = document.getElementById("menu_content")
+    let y = document.getElementById("content")
+
+    if(x.style.display == 'none'){
+        x.style.display = 'block'
+        y.style.display = "none"
+    }else{
+        x.style.display = 'none'
+        y.style.display = "block"
+    }
+    
 }
+
+
