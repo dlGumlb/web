336e5c07047ce2f5ca1611f2f5a5597ff5358acf
Eason T <easont368777@gmail.com>
2024-04-12 09:08:26 -0400

    change

--- static/js/f.js
+++ static/js/f.js
@@ -18,8 +18,21 @@ function openmenu() {
 
 
 function open_card(number){
+    let x=0, y=0, z=0, a=0;
     if(number == 1){
+        if(x=0){
+            let Element = document.getElementById("one")
+            Element.classList.remove("not_turn")
+            Element.classList.add("card")   
+        }else{
+            let Element = document.getElementById("one")
+            Element.classList.remove("card") 
+            Element.classList.add("not_turn")  
+        }
         
+    }else if(number == 2){
+        let Element = document.getElementById("two")
+        Element.classList.add("card")
     }
 }
 
